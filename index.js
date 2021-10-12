6ce103e7dd50d34ed9965c27339b2994d05abb63
Nick Persad <nick_persad@outlook.com>
2021-10-11 23:38:31 -0400

    Add query, skip, take

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const port = 3000;
 const axios = require("axios");
 
 const getPublix = async () => {
+  // Make a request to the publix servics endpoint to get the closest location
+  const location = await axios.get(
+    "https://services.publix.com/api/v1/storelocation",
+    {
+      params: {
+        types: "R,G,H,N,S",
+        option: "",
+        count: "15",
+        includeOpenAndCloseDates: "true",
+        isWebsite: "true",
+        zipCode: `33073`,
+      },
+    }
+  );
+  const [closest] = location.data.Stores;
+  console.log(closest.SHORTNAME);
   const url = `https://services.publix.com/api/v4/savings?page=1`;
   try {
     const response = await axios.get(url);
@@ -13,14 +29,15 @@ const getPublix = async () => {
         title: item.title,
         description: item.description,
         savings: item.savings,
-        store: `Publix`
+        store: `Publix`,
+        image: item.imageUrl,
       };
     });
     return clean;
   } catch (error) {
     console.error(error);
   }
-}
+};
 
 const getWinnDixie = async () => {
   const url = `https://dam.flippenterprise.net/flyerkit/publication/4399432/products?display_type=all&locale=en&access_token=144f255172b672dfe5bd75d2e8fb126a`;
@@ -31,15 +48,20 @@ const getWinnDixie = async () => {
       return {
         title: item.name,
         description: item.description,
-        savings: `${item.pre_price_text}${item.price_text}${item.pre_price_text}`,
-        store: `Winn-Dixie`
+        price: `${item.pre_price_text}${item.price_text}${item.pre_price_text}`,
+        store: `Winn-Dixie`,
+        image: item.large_image_url,
       };
     });
     return clean;
   } catch (error) {
     console.error(error);
   }
-}
+};
+
+const paginate = (results, offset, rowcount) => {
+  return results.slice(offset, offset + rowcount);
+};
 
 app.get("/", async (req, res) => {
   res.send(`Forbidden`);
@@ -55,14 +77,25 @@ app.get("/api/winndixie", async (req, res) => {
   res.send(store);
 });
 
-app.get("/api/search", async ({query}, res) => {
-  let { q, skip = 0, take = 10, sort = 'asc', companies = '',  titles = ''} = query;
+app.get("/api/search", async ({ query }, res) => {
+  let { q, skip = 0, take = 10 } = query;
   const publix = await getPublix();
   const winndixie = await getWinnDixie();
-  const combined = [...publix, ...winndixie];
-  const filtered = combined.filter(r => r.title.toLowerCase().includes(q.toLowerCase()));
-  // const filtered = combined.filter(value => value.title.toLowerCase())
-  res.send(filtered);
+  let data = [...publix, ...winndixie];
+
+  skip = parseInt(skip, 10);
+  take = parseInt(take, 10);
+
+  if (q) {
+    data = data.filter((r) =>
+      r.title.toLowerCase().includes(q.toLowerCase())
+    );
+  }
+
+  res.json({
+    count: data.length,
+    results: paginate(data, skip, take),
+  });
 });
 
 app.listen(port, () => {});
